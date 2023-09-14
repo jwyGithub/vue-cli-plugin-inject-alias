e8188012cc6425a7db80080f861311ed07e4dc0f
jiangwy <jiangwy@staryea.com>
2023-09-14 14:24:23 +0800

    deps: update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,8 @@ module.exports = {
         '@typescript-eslint/no-floating-promises': 'off',
         'consistent-return': 'off',
         '@typescript-eslint/no-unsafe-argument': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off'
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/unbound-method': 'off'
     }
 };
 
